891de9dec30aed4ab0152c70c28c118bd378b67c
Stanley Darmawan <stanmation@gmail.com>
2026-03-08 11:41:48 +1100

    Minor improvement on text styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -236,10 +236,10 @@ function Home() {
               darkMode ? 'opacity-100' : 'opacity-0'
             }`}
           />
-          <div className="absolute inset-0 bg-black bg-opacity-50" />
+          <div className="absolute inset-0 bg-white dark:bg-black bg-opacity-50" />
         </div>
         
-        <div className="relative container text-center text-white space-y-8">
+        <div className="relative container text-center text-gray-800 dark:text-white space-y-8">
 
           <motion.h1 
             className="text-3xl font-bold md:text-4xl lg:text-6xl"
